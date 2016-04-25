8db63399f8bf551b2c13508f853de27220b77267
YamaCIR-KIT <httperror@404-notfound.jp>
2016-04-26 03:26:56 +0900

    Add .gitconfig

--- sh/link.sh
+++ sh/link.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 # create linkers to dotfiles
 
-ln -sf ~/dotfiles/.Xdefaults ~/.Xdefaults
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.dircolors ~/.dircolors
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.inputrc ~/.inputrc
 ln -sf ~/dotfiles/.latexmkrc ~/.latexmkrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.vim/ ~/.vim/
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.Xdefaults ~/.Xdefaults
