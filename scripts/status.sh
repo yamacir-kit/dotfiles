28a06a1dc17feace207c404340d42557bd954b93
YamaCIR-KIT <httperror@404-notfound.jp>
2016-07-29 15:24:30 +0900

    後のお手入れのためワンライナー廃止．

--- scripts/status.sh
+++ scripts/status.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-cd `tmux display-message -p -F "#{pane_current_path}"`
+CURRENT_PATH=$(tmux display-message -p -F "#{pane_current_path}")
+
+cd $CURRENT_PATH
+
+BRANCH_NAME=$(git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')
+CHANGES_COUNT=$(git status -s | wc -l)
 
 if git status &> /dev/null; then
-  echo -e "$(git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')($(git status -s | wc -l)) ";
+  echo -e "$BRANCH_NAME($CHANGES_COUNT)";
 else
   echo "not a git repository";
 fi
