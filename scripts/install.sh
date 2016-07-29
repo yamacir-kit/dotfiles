c38c5c197754e14f8504dae1be4f02e92a0b4681
YamaCIR-KIT <httperror@404-notfound.jp>
2016-07-29 17:25:58 +0900

    リンク用スクリプト改良．リンク対象のファイルと同じ階層で実行する必要あり．

---
+++ scripts/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+for f in $(pwd)/.??*
+do
+  test "$f" = "$(pwd)/.git" && continue
+  ln -sf "$f" "$HOME"
+done
+
+# if [ ! -e /etc/X11/xorg.conf.d ]; then
+#   mkdir /etc/X11/xorg.conf.d
+# fi
+# ln -sf ../50-synaptics.conf /etc/X11/xorg.conf.d/50-synaptics.conf
+
+exit 0
