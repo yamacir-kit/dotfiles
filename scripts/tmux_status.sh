ba8b5005febf9b41b34c39a5f2f67159fd01e3ca
YamaCIR-KIT <httperror@404-notfound.jp>
2016-10-23 04:28:15 +0900

    大文字は環境変数とすることにしたから、ただの変数は小文字へ

--- scripts/tmux_status.sh
+++ scripts/tmux_status.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-CURRENT_PATH=$(tmux display-message -p -F "#{pane_current_path}")
+current_path=$(tmux display-message -p -F "#{pane_current_path}")
 
-cd $CURRENT_PATH
+cd $current_path
 
-BRANCH_NAME=$(git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')
-CHANGES_COUNT=$(git status -s | wc -l)
+branch_name=$(git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')
+changes_count=$(git status -s | wc -l)
 
-if git status &> /dev/null; then
-  echo -e "$BRANCH_NAME($CHANGES_COUNT)";
+if git status &> /dev/null
+then
+  echo -e "$branch_name($changes_count)";
 else
   echo "not a git repository";
 fi
