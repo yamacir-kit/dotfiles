0a6c6c3a76ffe29fab84a5e83c6564f4b2605eff
YamaCIR-KIT <httperror@404-notfound.jp>
2016-10-23 03:15:25 +0900

    ちょろめのインストールを追加

--- scripts/setup_ubuntu.sh
+++ scripts/setup_ubuntu.sh
@@ -14,9 +14,16 @@ sudo apt-get -y install $devpkgs $devenvs $texpkgs
 
 source $dotfiles/install.sh
 
-# -- Theme and icons ------------------
+
+# -- Theme and icons -------------------------------------------
 #  theme: Numix
 #  icons: Numix-circle
-# --------------------------------------
+# --------------------------------------------------------------
 sudo apt-add-repository -y ppa:numix/ppa && apt-get update
 sudo apt-get -y install numix-gtk-theme numix-icon-theme-circle unity-tweak-tool
+
+
+# -- Google Chrome ---------------------------------------------
+sudo wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+sudo apt-get update && sudo apt-get -y install google-chrome-stable
