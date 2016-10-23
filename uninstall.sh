5fffdff0726f7c07ec95b0d3b5ffcc6e9216f9be
YamaCIR-KIT <httperror@404-notfound.jp>
2016-10-23 15:24:30 +0900

    アンインストールをどこからでも実行できるように変更

--- uninstall.sh
+++ uninstall.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+dotfiles=$(cd "$(dirname $0)"; pwd)
+cd $dotfiles || exit
+
 for f in .??*
 do
-  test "$f" = ".git" && continue
-  test "$f" = ".gitignore" && continue
-  unlink "$HOME/$f"
+  test $f = ".git" && continue
+  test $f = ".gitignore" && continue
+  unlink $HOME/$f
 done
