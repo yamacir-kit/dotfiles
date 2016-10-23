f1e07a109e44d952fcd854dc0c26290d5329cf3e
YamaCIR-KIT <httperror@404-notfound.jp>
2016-10-24 01:56:12 +0900

    YCMの設定ファイルの場所に意味がないことが分かったためいろいろ変更
    シンボリックリンクは削除

--- scripts/install_vimycm.sh
+++ scripts/install_vimycm.sh
@@ -4,6 +4,3 @@ dotfiles=$(cd "$(dirname $0)/.."; pwd)
 
 python2 $dotfiles/.vim/bundle/YouCompleteMe/install.py --clang-completer --system-libclang
 # python2 $dotfiles/.vim/bundle/YouCompleteMe/install.py --all --system-libclang
-
-cd $dotfiles/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ycm
-ln -sf $dotfiles/.vim/config/.ycm_extra_conf.py
