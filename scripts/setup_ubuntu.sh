31194437a86f9d7b86ffa0d066cb7be6ab509655
YamaCIR-KIT <httperror@404-notfound.jp>
2016-12-04 03:31:38 +0900

    Delete dropbox and skype
    単にaptで入らない上，詳細なインストール手順を失念したため

--- scripts/setup_ubuntu.sh
+++ scripts/setup_ubuntu.sh
@@ -7,10 +7,9 @@ libpkgs="libclang-3.8-dev"
 devpkgs="build-essential clang cmake git llvm python-dev python-flake8 python3-dev"
 devenvs="rxvt-unicode-256color tmux"
 texpkgs="dvipsk-ja gv latexmk pstoedit texlive texlive-fonts-extra texlive-fonts-recommended texlive-lang-cjk xdvik-ja"
-others="dropbox skype "
 
 sudo apt-get update && sudo apt-get dist-upgrade
-sudo apt-get -y install $libpkgs $devpkgs $devenvs $texpkgs $others
+sudo apt-get -y install $libpkgs $devpkgs $devenvs $texpkgs
 
 # source $dotfiles/install.sh
 
