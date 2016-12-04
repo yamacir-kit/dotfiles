f502417911bef3aa63551894a0b76032a5944c42
YamaCIR-KIT <httperror@404-notfound.jp>
2016-12-04 15:02:14 +0900

    Fix bug?

--- scripts/setup_ubuntu.sh
+++ scripts/setup_ubuntu.sh
@@ -23,8 +23,8 @@ sudo apt-get -y install numix-gtk-theme numix-icon-theme-circle unity-tweak-tool
 
 
 # -- Google Chrome ---------------------------------------------
-sudo wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
 sudo apt-get update && sudo apt-get -y install google-chrome-stable
 
 
