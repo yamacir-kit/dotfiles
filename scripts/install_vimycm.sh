9b1a8e3da24a29771286a8979b6aad7156538a33
yamacir-kit <httperror@404-notfound.jp>
2018-09-08 17:42:40 +0900

    Remove unneeded files
    作ってから使った試しがない

--- scripts/install_vimycm.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-dotfiles=$(cd "$(dirname $0)/.."; pwd)
-
-python2 $dotfiles/.vim/bundle/YouCompleteMe/install.py --clang-completer --system-libclang
-# python2 $dotfiles/.vim/bundle/YouCompleteMe/install.py --all --system-libclang
--- scripts/setup_workspace.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-scripts=$(cd "$(dirname $0)"; pwd)
-dotfiles="$scripts/.."
-
-ln -sf ~/Dropbox/works $HOME
