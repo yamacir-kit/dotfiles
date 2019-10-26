02483c3ea7b66f185f25a3aff05febd700bb532a
yamacir-kit <httperror@404-notfound.jp>
2019-10-26 13:51:42 +0900

    Add Ubuntu 18.04 setup script

---
+++ scripts/setup-ubuntu-18.04.bash
@@ -0,0 +1,74 @@
+#!/bin/bash -eux
+
+scripts=$(cd "$(dirname $0)/.."; pwd)
+
+sudo apt-add-repository -y ppa:numix/ppa
+sudo apt update
+
+# command
+packages="\
+  tree \
+"
+
+# develop
+packages="$packages \
+  build-essential \
+  clang \
+  cmake \
+  g++ \
+  gcc \
+  git \
+  git-lfs \
+  tmux \
+"
+
+# library
+packages="$packages \
+  libboost-all-dev \
+  libcairo2-dev \
+  libgmp-dev \
+  libmpfr-dev \
+  libx11-dev \
+  libxext-dev \
+  libxft-dev \
+"
+
+# documentation
+packages="$packages \
+  gnuplot \
+  gv \
+  latexmk \
+  pstoedit \
+  texlive \
+  texlive-fonts-extra \
+  texlive-fonts-recommended \
+  texlive-lang-cjk \
+  xdvik-ja \
+"
+
+# miscellaenous
+packages="$packages \
+  gimp \
+  indicator-cpufreq \
+  inkscape \
+"
+
+# numix
+packages="$packages \
+  gnome-tweak-tool \
+  numix-gtk-theme \
+  numix-icon-theme-circle \
+  numix-icon-theme-square \
+"
+
+# fonts
+package="$packages \
+  fonts-firacode \
+  fonts-noto-cjk \
+  fonts-noto-cjk-extra \
+  fonts-noto-color-emoji \
+  fonts-noto-mono \
+"
+
+sudo apt install -y $packages
+
