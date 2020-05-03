f3aa6de6656ba542203cc8c6a2b9f704654750dd
yamacir-kit <httperror@404-notfound.jp>
2020-05-04 05:16:16 +0900

    Update Vim install script for YouCompleteMe latest

---
+++ scripts/setup-vim.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -eux
+
+current_directory=$(dirname "$0")
+
+dotfiles=$(cd "$current_directory/.."; pwd)
+
+sudo apt-add-repository -y ppa:jonathonf/vim
+sudo apt update
+
+sudo apt install -y vim-gnome build-essential cmake python3-dev
+
+bundle=$dotfiles/.vim/bundle
+
+mkdir -p "$bundle"
+if test ! -e "$bundle/Vundle.vim"
+then
+  git clone https://github.com/VundleVim/Vundle.vim.git "$bundle/Vundle.vim"
+fi
+
+vi -c PluginInstall -c qa
+
+python3 "$bundle/YouCompleteMe/install.py" --clangd-completer
+
+sudo apt remove -y vim-tiny
+
