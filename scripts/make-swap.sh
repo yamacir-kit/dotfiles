dba18bf052b46d928a8f5a9b8044f5b709b73335
yamacir-kit <httperror@404-notfound.jp>
2020-05-04 05:11:01 +0900

    Update script make-swap to be POSIX compat

--- scripts/make-swap.sh
+++ scripts/make-swap.sh
@@ -2,7 +2,7 @@
 
 name="/tmp/swap"
 
-sudo fallocate -l $@ $name # sudo fallocate -l 8G /swap
+sudo fallocate -l "$@" $name # sudo fallocate -l 8G /swap
 sudo chmod 600 $name
 sudo mkswap $name
 sudo swapon $name
