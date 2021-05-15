061f508619b490da33e81b1568515787c1c58359
yamacir-kit <httperror@404-notfound.jp>
2021-05-15 12:48:33 +0900

    Fix bug

--- uninstall.sh
+++ uninstall.sh
@@ -2,9 +2,11 @@
 
 here=$(cd "$(dirname "$0")"; pwd)
 
+cd "$here"
+
 for f in .??*
 do
-  test "$f" = "$here"/.git && continue
-  test "$f" = "$here"/.gitignore && continue
+  test "$f" = .git && continue
+  test "$f" = .gitignore && continue
   unlink "$HOME/$f"
 done
