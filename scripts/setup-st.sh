8dfc82894a6592e4b1fe7631a5e923d25056c219
yamacir-kit <httperror@404-notfound.jp>
2024-05-14 02:30:53 +0900

    Add patch file `st.patch`
    Signed-off-by: yamacir-kit <httperror@404-notfound.jp>

--- scripts/setup-st.sh
+++ scripts/setup-st.sh
@@ -1,11 +1,15 @@
 #!/bin/sh -eu
 
+dotfiles=$(cd "$(dirname "$0")/.."; pwd)
+
 sudo apt update
 
 sudo apt install libx11-dev libxft-dev
 
-git clone https://git.suckless.org/st
+git clone https://git.suckless.org/st "$dotfiles"/st
 
-cd st
+cd "$dotfiles"/st
+
+git apply "$dotfiles"/scripts/st.patch
 
 sudo make install
