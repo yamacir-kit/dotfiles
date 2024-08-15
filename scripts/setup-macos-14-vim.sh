c770cf2a0808cfdfa7c8c470cdedf8306e229b7c
yamacir-kit <httperror@404-notfound.jp>
2024-08-15 15:06:28 +0900

    Update `setup-macos-14-vim.sh`
    Signed-off-by: yamacir-kit <httperror@404-notfound.jp>

--- scripts/setup-macos-14-vim.sh
+++ scripts/setup-macos-14-vim.sh
@@ -3,3 +3,18 @@
 brew update
 
 brew install cmake python vim
+
+bundle=$(dirname "$0")/../.vim/bundle
+
+mkdir -p "$bundle"
+
+if test ! -e "$bundle/Vundle.vim"
+then
+  git clone https://github.com/VundleVim/Vundle.vim "$bundle/Vundle.vim"
+fi
+
+vi -c PluginInstall -c qa
+
+cd $bundle/YouCompleteMe
+
+python3 ./install.py --clangd-completer
