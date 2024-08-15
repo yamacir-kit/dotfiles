bd75b5dd0a14f745ff5434a8aeaedc5915b830b6
yamacir-kit <httperror@404-notfound.jp>
2024-08-15 15:26:09 +0900

    Update `setup-macos-14.sh`
    Signed-off-by: yamacir-kit <httperror@404-notfound.jp>

--- scripts/setup-macos-14.sh
+++ scripts/setup-macos-14.sh
@@ -1,3 +1,30 @@
 #!/bin/sh
 
 brew update
+
+brew install tmux google-chrome
+
+default()
+{
+  echo google-chrome
+  echo tmux
+}
+
+optional()
+{
+}
+
+if test "$#" -eq 0
+then
+  default | xargs brew install
+else
+  for each in "$@"
+  do
+    case "$each" in
+      -a | --all      ) ( default && optional ) | xargs sudo brew install ;;
+      -d | --default  ) ( default             ) | xargs sudo brew install ;;
+      -o | --optional ) (            optional ) | xargs sudo brew install ;;
+    esac
+  done
+fi
+
