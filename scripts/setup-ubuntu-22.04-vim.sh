1617c386c53b25283d8631d111464229bce6b2f5
yamacir-kit <httperror@404-notfound.jp>
2024-10-11 01:04:54 +0900

    Update script `setup-ubuntu-22.04-vim.sh` to install Vim from source
    Signed-off-by: yamacir-kit <httperror@404-notfound.jp>

--- scripts/setup-ubuntu-22.04-vim.sh
+++ scripts/setup-ubuntu-22.04-vim.sh
@@ -1,26 +1,63 @@
 #!/bin/sh -eu
 
-sudo apt update
-
-sudo apt install software-properties-common # for apt-add-repository
-
-sudo apt-add-repository -y ppa:jonathonf/vim
+dotfiles=$(cd "$(dirname "$0")/.."; pwd)
 
 sudo apt update
+sudo apt install build-essential gettext git libacl1-dev libgpm-dev libtinfo-dev libxmu-dev libxpm-dev # https://vim-jp.org/docs/build_linux.html
+sudo apt remove vim-tiny
+sudo apt autoremove
+sudo apt autoclean
 
-sudo apt install -y build-essential cmake python3-dev vim-gtk3
+vim=$dotfiles/vim
 
-bundle=$(dirname "$0")/../.vim/bundle
-
-mkdir -p "$bundle"
-
-if test ! -e "$bundle/Vundle.vim"
+if test ! -d "$vim"
 then
-  git clone https://github.com/VundleVim/Vundle.vim "$bundle/Vundle.vim"
+  git clone https://github.com/vim/vim.git "$vim"
+  cd "$vim"
+else
+  cd "$vim"
+  if test -d "$vim/Makefile"
+  then
+    make uninstall
+    make clean
+    git clean -fdx
+    git pull
+  fi
 fi
 
-vi -c PluginInstall -c qa
+./configure \
+  --enable-multibyte \
+  --enable-python3interp \
+  --with-features=huge \
+  --with-x \
 
-cd $bundle/YouCompleteMe
+make -j
+
+sudo make install
+
+sudo update-alternatives --install "/usr/bin/vim" "vim" "/usr/local/bin/vim" 1
+sudo update-alternatives --install "/usr/bin/vi"  "vi"  "/usr/local/bin/vim" 1
+sudo update-alternatives --config vim
+sudo update-alternatives --config vi
+
+vim --version
+
+cd "$dotfiles"
+
+bundle=$dotfiles/.vim/bundle
+
+if test ! -d "$bundle"
+then
+  mkdir -p "$bundle"
+  if test ! -e "$bundle/Vundle.vim"
+  then
+    git clone https://github.com/VundleVim/Vundle.vim "$bundle/Vundle.vim"
+  fi
+fi
+
+vim -c PluginInstall -c qa
+vim -c PluginUpdate -c qa
+
+cd "$bundle/YouCompleteMe"
 
 python3 ./install.py --clangd-completer
